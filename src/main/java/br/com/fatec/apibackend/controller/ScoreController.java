622e917aefb003e2dffba93148cefdde04524bf8
Marcos Paulo <marcos.simoes@geopx.com.br>
2021-11-29 09:48:40 -0300

    Opa

--- src/main/java/br/com/fatec/apibackend/controller/ScoreController.java
+++ src/main/java/br/com/fatec/apibackend/controller/ScoreController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,10 +29,11 @@ public class ScoreController {
   private ScoreRepository scoreRepo;
 
   @JsonView(ViewUsuario.UsuarioView.class)
-  @RequestMapping("/{id}")
+  @PostMapping("/{id}")
   public ResponseEntity<Score> cadastra(@PathVariable long id, @RequestBody String cluster,
       @RequestBody String fingerPrint, @RequestBody String traceRouter) {
     Score score = new Score();
+    System.out.println(fingerPrint);
     score.setCluster(Integer.parseInt(cluster));
     score.setFingerPrint(fingerPrint);
     score.setTraceRouter(traceRouter);
